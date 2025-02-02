72722bf59de39c749ecb2a84b1895ddb00368ce4
Yolisses <yolisses0@gmail.com>
2025-02-02 20:39:37 -0300

    Create graph buffer

--- src/graph.rs
+++ src/graph.rs
@@ -1,7 +1,12 @@
 use wasm_bindgen::prelude::wasm_bindgen;
 
+// This value is based on the audio context block size. It should probably be
+// replaced later.
+const BUFFER_SIZE: usize = 128;
+
 #[wasm_bindgen]
 pub struct Graph {
+    buffer: [f32; BUFFER_SIZE],
     graph: modulee_engine::graph::Graph,
 }
 
@@ -29,12 +34,18 @@ impl Graph {
     #[wasm_bindgen(constructor)]
     pub fn new() -> Self {
         Self {
+            buffer: [0.; BUFFER_SIZE],
             graph: modulee_engine::graph::Graph::new(),
         }
     }
 
     #[wasm_bindgen]
-    pub fn process_block(&mut self, buffer: &mut [f32], length: usize) {
-        self.graph.process_block(buffer, length);
+    pub fn get_buffer_pointer(&self) -> *const f32 {
+        self.buffer.as_ptr()
+    }
+
+    #[wasm_bindgen]
+    pub fn process_block(&mut self) {
+        self.graph.process_block(&mut self.buffer, BUFFER_SIZE);
     }
 }
