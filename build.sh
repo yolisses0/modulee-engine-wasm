2aea907cfb7da71f3f22c8dc2086ef45752294ec
Yolisses <yolisses0@gmail.com>
2025-02-02 19:24:13 -0300

    Add wasm pack building in build.sh

--- build.sh
+++ build.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
-# Define the file path
-file_path="pkg/modulee_engine_wasm_bg.js"
+wasm-pack build &&
+    (
+        # Define the file path
+        file_path="pkg/modulee_engine_wasm_bg.js"
 
-# Define the import statement
-import_statement='import {
+        # Define the import statement
+        import_statement='import {
     TextDecoder,
     TextEncoder,
 } from "./text-encoder-and-decoder-polyfill.js";
 
 '
 
-# Create a temporary file with the import statement and the original content
-{
-    echo "$import_statement"
-    cat "$file_path"
-} >"${file_path}.tmp"
+        # Create a temporary file with the import statement and the original content
+        {
+            echo "$import_statement"
+            cat "$file_path"
+        } >"${file_path}.tmp"
 
-# Replace the original file with the temporary file
-mv "${file_path}.tmp" "$file_path"
+        # Replace the original file with the temporary file
+        mv "${file_path}.tmp" "$file_path"
+    )
