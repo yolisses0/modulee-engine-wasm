20fba10ee396b6a6fa09fb788fb09360c7e68935
Yolisses <iamurise@gmail.com>
2025-02-03 19:07:07 -0300

    Update modulee-engine

--- src/graph.rs
+++ src/graph.rs
@@ -13,18 +13,6 @@ pub struct Graph {
 #[wasm_bindgen]
 impl Graph {
     #[wasm_bindgen]
-    pub fn get_debug_value(&self) -> f32 {
-        self.graph.get_debug_value()
-    }
-
-    #[wasm_bindgen]
-    pub fn set_debug_string(&mut self, debug_string: String) {
-        self.graph.set_debug_string(&debug_string);
-        let string_value = self.graph.get_debug_string();
-        log::debug!("string value: {}", string_value);
-    }
-
-    #[wasm_bindgen]
     pub fn set_nodes_from_json(&mut self, nodes_data_json: String) {
         if let Err(e) = self.graph.set_nodes_from_json(&nodes_data_json) {
             log::error!("Failed to set nodes from JSON: {}\n{}", e, nodes_data_json);
