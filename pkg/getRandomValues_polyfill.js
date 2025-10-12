2d790400977e2f9c4e1886cb0966e3e5628e934e
Yolisses <yolisses0@gmail.com>
2025-10-12 08:09:18 -0300

    Use more consistent file names for polyfills

--- pkg/getRandomValues_polyfill.js
+++
@@ -1,9 +0,0 @@
-globalThis.crypto = {
-  getRandomValues(abv) {
-    let counter = abv.length;
-    while (counter--) {
-      abv[counter] = Math.floor(Math.random() * 256);
-    }
-    return abv;
-  },
-};
